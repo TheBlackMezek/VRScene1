de0f9b4fe19072b73fc3eaab236b2bf12161287c
Alexander Thomas <6ftDwarf@gmail.com>
2018-09-18 15:51:12 -0700

    Add gravity slider to grav room

---
+++ Assets/Experiments/GravityRoom/Scripts/CR_GravSetter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables;
+
+public class CR_GravSetter : MonoBehaviour {
+
+    [SerializeField]
+    private VRTK_BaseControllable controllable;
+
+    private float initialGrav;
+
+
+
+    private void Awake()
+    {
+        initialGrav = Physics.gravity.y;
+    }
+
+    private void OnEnable()
+    {
+        controllable.ValueChanged += OnValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        controllable.ValueChanged -= OnValueChanged;
+    }
+
+    private void OnValueChanged(object sender, ControllableEventArgs e)
+    {
+        Physics.gravity = new Vector3(0, initialGrav * e.value, 0);
+    }
+
+}
