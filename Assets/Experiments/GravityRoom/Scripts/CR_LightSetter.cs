21f413dabc726e888ccf6a9dd39c438cf19469f3
Alexander Thomas <6ftDwarf@gmail.com>
2018-09-18 15:28:56 -0700

    Add gravity room structure & light slider

---
+++ Assets/Experiments/GravityRoom/Scripts/CR_LightSetter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables;
+
+public class CR_LightSetter : MonoBehaviour {
+
+    [SerializeField]
+    private VRTK_BaseControllable controllable;
+    [SerializeField]
+    private Light[] lights;
+
+    private float[] initialIntensities;
+
+
+
+    private void Awake()
+    {
+        initialIntensities = new float[lights.Length];
+
+        for (int i = 0; i < lights.Length; ++i)
+        {
+            initialIntensities[i] = lights[i].intensity;
+        }
+    }
+
+    private void OnEnable()
+    {
+        controllable.ValueChanged += OnValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        controllable.ValueChanged -= OnValueChanged;
+    }
+
+    private void OnValueChanged(object sender, ControllableEventArgs e)
+    {
+        Debug.Log(e.value);
+        for(int i = 0; i < lights.Length; ++i)
+        {
+            lights[i].intensity = Mathf.Lerp(initialIntensities[i], 0f, e.value);
+        }
+    }
+
+}
