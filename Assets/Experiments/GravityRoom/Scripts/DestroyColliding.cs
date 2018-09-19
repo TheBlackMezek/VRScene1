a3307b5e4e07479df98b7a6b1f8ed15f69d7502c
Alexander Thomas <6ftDwarf@gmail.com>
2018-09-18 17:01:01 -0700

    Add incinerator to grav room

---
+++ Assets/Experiments/GravityRoom/Scripts/DestroyColliding.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyColliding : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+}
