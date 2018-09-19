3ddc771de50803f2fbbc94c9d426f12678a9862b
Alexander Thomas <6ftDwarf@gmail.com>
2018-09-18 17:58:27 -0700

    Add object spawner to grav room

---
+++ Assets/Experiments/GravityRoom/Scripts/CR_SpawnObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables;
+
+public class CR_SpawnObject : MonoBehaviour {
+
+    [SerializeField]
+    private VRTK_BaseControllable controllable;
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private Transform spawnPoint;
+
+
+
+    private void OnEnable()
+    {
+        controllable.MaxLimitReached += OnMaxLimitReached;
+    }
+
+    private void OnDisable()
+    {
+        controllable.MaxLimitReached -= OnMaxLimitReached;
+    }
+
+    private void OnMaxLimitReached(object sender, ControllableEventArgs e)
+    {
+        GameObject obj = Instantiate(prefab);
+        prefab.transform.position = spawnPoint.position;
+    }
+
+}
