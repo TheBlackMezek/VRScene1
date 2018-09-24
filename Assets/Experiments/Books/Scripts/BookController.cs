e8463daff5c007f36bd2b507505fdeb7e0b6e634
Alexander Thomas <6ftDwarf@gmail.com>
2018-09-24 12:15:52 -0700

    Attempt to keep books closed

---
+++ Assets/Experiments/Books/Scripts/BookController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookController : MonoBehaviour {
+
+    [SerializeField]
+    private float closeThreshold;
+    [SerializeField]
+    private bool startsClosed;
+
+    [SerializeField]
+    private VRTK.VRTK_InteractableObject interactable;
+    [SerializeField]
+    private VRTK.Controllables.PhysicsBased.VRTK_PhysicsRotator frontRotator;
+
+    private bool closed;
+
+
+
+    private void Awake()
+    {
+        frontRotator.ValueChanged += OnValueChanged;
+        interactable.InteractableObjectUsed += OnBookUsed;
+    }
+
+    private void Start()
+    {
+        closed = startsClosed;
+        if (closed)
+        {
+            frontRotator.SetValue(180f);
+        }
+        frontRotator.isLocked = startsClosed;
+    }
+
+    private void OnValueChanged(object sender, VRTK.Controllables.ControllableEventArgs e)
+    {
+        if (e.value >= closeThreshold)
+        {
+            closed = true;
+            frontRotator.SetValue(180f);
+            frontRotator.isLocked = true;
+        }
+    }
+
+    private void OnBookUsed(object sender, VRTK.InteractableObjectEventArgs e)
+    {
+        if(closed)
+        {
+            closed = false;
+            frontRotator.isLocked = false;
+        }
+    }
+
+}
